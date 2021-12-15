c542de6b7379c8cc2e5aad77088cdb800489da58
sabrinaacham <sabrinaacham@gmail.com>
2021-12-15 00:07:04 -0400

    html

---
+++ script.js
@@ -0,0 +1,5 @@
+//For this assignment we will create a web page which works like the popular Mad Libs game.
+//HTML and CSS code is provided for the game 'Talk Like a Pirate' (web site, code) and you are required to write JavaScript which will insert words into the HTML using DOM manipulation.
+//The game has six types of word classes defined including those for 'noun', 'adjective', 'verb', 'adverb', 'plural-noun' and 'body-part'.
+//Part 1: Create arrays of 10 words each for the six classes outlined. Use a DOM query to return a list of elements based on the class names provided.
+//For each of the NodeLists returned, loop through the list and replace the text content with the matching type from the arrays you created
